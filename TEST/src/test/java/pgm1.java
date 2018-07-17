2f086840ec19dba3bbd7b57ce813348526ad3f36
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-3VWLX52.dir.svc.accenture.com>
2018-07-17 12:42:30 +0530

    first commit

---
+++ TEST/src/test/java/pgm1.java
@@ -0,0 +1,73 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class pgm1
+{
+    @Test
+	public void method1()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\GITArtifacts\\chromedriver.exe");
+    	WebDriver driver=new ChromeDriver();
+    	driver.get("https://github.com/login");
+    	driver.manage().window().maximize();
+    	
+    	String name;
+		driver.findElement(By.name(name="login")).click();
+		driver.findElement(By.name(name="login")).sendKeys("pavi-01");
+    	driver.findElement(By.name(name="password")).sendKeys("Admin2018");
+    	driver.findElement(By.name(name="commit")).click();
+    	
+    	String title=driver.getTitle();
+    	System.out.println(title);
+    	
+    	Assert.assertEquals(title,"Sign in to GitHub. GitHub");
+    	
+    	System.out.println("Continue to test...");
+    	
+    	
+    	
+    	
+		
+	}
+	
+}  
+
+
+
+/*public class pgm1
+{
+    @Test
+	public void method1()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\GITArtifacts\\chromedriver.exe");
+    	WebDriver driver=new ChromeDriver();
+    	driver.get("https://github.com/login");
+    	driver.manage().window().maximize();
+    	
+    	String name;
+		driver.findElement(By.name(name="login")).click();
+		driver.findElement(By.name(name="login")).sendKeys("pavi-01");
+    	driver.findElement(By.name(name="password")).sendKeys("Admin2018");
+    	driver.findElement(By.name(name="commit")).click();
+    	
+    	String title=driver.getTitle();
+    	System.out.println(title);
+    	
+    	Assert.assertEquals(title,"Sign in to GitHub. GitHub");
+    	
+    	System.out.println("Continue to test...");
+    	
+    	
+    	
+    	
+		
+	}
+	
+}  */
+
+
